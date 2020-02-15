f042e94fb8250dae6b3ebdc12757d42a9f549c40
VladGavra98 <43482835+VladGavra98@users.noreply.github.com>
2020-02-15 14:29:15 +0100

    Update main_sim.py

--- main_sim.py
+++ main_sim.py
@@ -2,23 +2,49 @@
 """
 SVV 2020- Structural Analysis Assignment
 
+Simulation for stress and deflection in A320 Airleron
 
 @author: vladg
 """
-
 import numpy as np
 import scipy as sp
 import scipy.integrate
 import matplotlib.pyplot as plt
-
+from matplotlib import collections  as mc
 aircraft = "A320"
 
-class Aileron(name):
-    """The class containing the geomtric parameters of the aileron and the material properties.
-    The name is a string that contains the name of the aircraft: A320, B737"""
-    def __init__(self,name):
-        self.la = 2.71  #m
-        self.ca = 0.5
-        self.ha = 0.22
-        self.ts = 0.0011
 
+def drawSection(ha,ca):
+    fig,ax = plt.subplots()
+    plt.title("Cross section")
+    lines = [[(0, 0), (ha/2, ha/2)], [(0, 0), (ha/2, -ha/2)], [(ha/2, ha/2), (ha/2, -ha/2)],
+             [(ha/2,ha/2),(ca,0)],[(ha/2,-ha/2),(ca,0)]]
+
+    lc = mc.LineCollection(lines, linewidths=2)
+    circle = plt.Circle((ha/2,0),ha/2,color='b',fill=False)
+    ax.add_artist(circle)
+    ax.add_collection(lc)
+    ax.autoscale()
+
+def calcCircum(ha,ca):
+    #Stick to the name given in the flow chart OR
+    # cleary write what you cahnged
+    """Input: ha, ca
+        Output: lCirc = length of the section circumference
+
+    """
+    return np.pi*ha/2 + 2*(np.sqrt((ca-ha/2)**2 + (ha/2)**2))
+
+
+def main(aircraft):
+    la = 2.71 #m
+    ca = 0.5
+    ha = 0.2
+    tsk = 0.00011
+    tsp = 0.0002
+    wst = 0.002
+    hst = 0.002
+    nst = 17
+    print("Circumference: \n",calcCircum(ha,ca))
+    drawSection(ha,ca)
+main(aircraft)
