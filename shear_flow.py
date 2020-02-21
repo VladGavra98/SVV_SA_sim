ea3da18840ec996de884f1f487d866dcf98c6a74
Danny Huang <huangdanny9@gmail.com>
2020-02-21 21:03:47 +0100

    added calcShFlowTorque in shear_flow.py

--- shear_flow.py
+++ shear_flow.py
@@ -14,50 +14,108 @@ from main_sim import *
 from integration import *
 
 def calcShCenter(ha,ca,tsk,tsp, tst, hst, wst,nst,n1,n2,n3,n4):
-    """Calculates Z coordinate of shear center"""
+    #---------------------------------------------------
+    # Calculates Z coordinate of shear center
+    #
+    # output:   description:                                                                                    type:
+    # zShear    z-coordinate of the shear center, origin on leading edge, trailing edge is negative direction   value
+    #
+    #---------------------------------------------------
+
     Sy = 1
     Sz = 0
     # open section shear flow
-    qs1,qs2,qs3,qs4,sVec1,sVec2,sVec3,sVec4 = calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4)
+    q1,q2,q3,q4,sVec1,sVec2,sVec3,sVec4 = calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4)
+
     # dimensions of upper and lower plate
     plateYLength = ha / 2
     plateZLength = ca - ha / 2
     plateLength = np.sqrt(np.power(plateYLength, 2) + np.power(plateZLength, 2))
-    # calculate const. sh flow
-    #rhs1 = -(-sp.integrate.simps(qs2, sVec2) / tsp + sp.integrate.simps(qs4, sVec4) / tsk)
-    #rhs2 = -(sp.integrate.simps(qs1, sVec1) / tsk + sp.integrate.simps(qs2, sVec2) / tsp + sp.integrate.simps(qs3,sVec3) / tsk)
-    rhs1 = -(-integrationArray(qs2, sVec2[0],sVec2[-1],n2) / tsp + integrationArray(qs4, sVec4[0],sVec4[-1],n4) / tsk)
-    rhs2 = -(integrationArray(qs1, sVec1[0],sVec1[-1],n1) / tsk + integrationArray(qs2, sVec2[0],sVec2[-1],n2) / tsp + integrationArray(qs3, sVec3[0],sVec3[-1],n3) / tsk)
 
-    A = np.matrix([[(np.pi * ha) / (2 * tsk) + (ha / tsp), -(ha / tsp)],
-                   [-(ha / tsp), (2 * plateLength) / (tsk) + (ha / tsp)]])
-    b = np.array([rhs1, rhs2])
-    # x[0] is q,0 of left cell, x[1] is q,0 of right cell
-    x = np.linalg.solve(A, b)
-    # add const. shear flow to open section shear flow to get final shear flow
-    q1 = qs1 + x[1]
-    q2 = qs2 + x[1] - x[0]
-    q3 = qs3 + x[1]
-    q4 = qs4 + x[0]
-
-    drawGraph(sVec1,q1)
     # calculate moment around center of semi circle to find moment arm
-    #zeta = sp.integrate.simps(q4, sVec4)*(ha / 2) + sp.integrate.simps(q1, sVec1) * (plateZLength / plateLength) * (ha / 2) + sp.integrate.simps(q3, sVec3) * (plateZLength / plateLength) * (ha / 2)
     zeta = integrationArray(q4, sVec4[0],sVec4[-1],n4)*(ha / 2) + integrationArray(q1 ,sVec1[0],sVec1[-1],n1) * (plateZLength / plateLength) * (ha / 2) + integrationArray(q3, sVec3[0],sVec3[-1],n3) * (plateZLength / plateLength) * (ha / 2)
+
+    # old zeta using scipy.integrate.simps()
+    # zeta = sp.integrate.simps(q4, sVec4)*(ha / 2) + sp.integrate.simps(q1, sVec1) * (plateZLength / plateLength) * (ha / 2) + sp.integrate.simps(q3, sVec3) * (plateZLength / plateLength) * (ha / 2)
+
     zShear = -zeta - (ha / 2)
+
     return zShear
 
 
-def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
-    """Calculate open section shear flow"""
+def calcShFlowTorque(ha,ca,tsk,tsp,G,T):
+    #---------------------------------------------------
+    # Calculate shear flow in both cells caused by torque
+    #
+    # output:   description:                                                                            type:
+    # q1        shear flow in upper plate, positive in direction of increasing y and increasing z       value
+    # q2        shear flow in spar, positive in direction of decreasing y                               value
+    # q3        shear flow in lower plate, positive in direction of decreasing y and decreasing z       value
+    # q4        shear flow in semicircle, positive in direction of decreasing y                         value
+    #
+    # q1, q2, q3, q4 are singular values that need to be added to the shear flow caused by shear forces acting through the shear center (calcShFlow)
+    #
+    #---------------------------------------------------
 
+    # geometry calculations
+    A1,A2 = calcCellArea(ha,ca)
+    plateYLength = ha / 2
+    plateZLength = ca - ha / 2
+    plateLength = np.sqrt(np.power(plateYLength, 2) + np.power(plateZLength, 2))
+
+    # setting up matrix equations
+    a11 = (1/(2*A1*G)) * ((np.pi*ha)/(2*tsk)+(ha/tsp))
+    a12 = -(1/(2*A1*G)) * (ha/tsp)
+    a13 = -1
+    a21 = -(1/(2*A2*G)) * (ha/tsp)
+    a22 = (1/(2*A2*G)) * ((2*plateLength)/(tsk)+(ha/tsp))
+    a23 = -1
+    a31 = 2*A1
+    a32 = 2*A2
+    a33 = 0
+    A = np.matrix([[a11,a12,a13],
+                   [a21,a22,a23],
+                   [a31,a32,a33]])
+    b = np.array([0,
+                  0,
+                  T])
+
+    # x[0]: q1 (constant shear flow left cell)
+    # x[1]: q2 (constant shear flow right cell)
+    # x[2]: dTheta/dx (twist rate)
+    # q1 and q2 are positive counter clockwise
+    x = np.linalg.solve(A,b)
+
+    q1 = x[1]
+    q2 = x[1]-x[0]
+    q3 = x[1]
+    q4 = x[0]
+
+    return q1,q2,q3,q4
+
+
+def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
+    #---------------------------------------------------
+    # Calculate shear flow, under condition that the shear forces act through the shear center
+    #
+    # output:   description:                                                                            type:
+    # q1        shear flow in upper plate, positive in direction of increasing y and increasing z       array[n1+1 , 1]
+    # q2        shear flow in spar, positive in direction of decreasing y                               array[n2+1 , 1]
+    # q3        shear flow in lower plate, positive in direction of decreasing y and decreasing z       array[n3+1 , 1]
+    # q4        shear flow in semicircle, positive in direction of decreasing y                         array[n4+1 , 1]
+    #
+    # q1, q2, q3, q4 are singular values that need to be added to the shear flow caused by shear forces acting through the shear center (calcShFlow)
+    #
+    #---------------------------------------------------
+
+    # geometry calculations
     zCentroid = calcCentroid(ha,ca,tsk,tsp,tst,hst,wst,nst)
     stArea = calcStArea(tst,hst,wst)
     stringerPos = calcStPose(ha, ca, nst)
     Izz,Iyy = calcInertia(ca, ha, tsk, tsp, tst, stArea, zCentroid, stringerPos)
 
-    stringerPosCentroid = stringerPos
     # calculate stringer coordinates w.r.t. centroid
+    stringerPosCentroid = stringerPos
     stringerPosCentroid[1, :] = stringerPosCentroid[1, :] - zCentroid
 
     # upper and lower flat plate lengths
@@ -65,7 +123,10 @@ def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
     plateZLength = ca-ha/2
     plateLength = np.sqrt(np.power(plateYLength, 2) + np.power(plateZLength, 2))
 
-    ###### (1) top flat plate : starting from (y = 0, z = -ca)
+    #---------------------------------------------------
+    # (1) top flat plate : starting from (y = 0, z = -ca)
+    #---------------------------------------------------
+
     # filter stringers of top flate plate
     stringerPosFilt1 = stringerPosCentroid[:, (stringerPosCentroid[0, :] >= 0) & (stringerPosCentroid[1, :] <= -(ha / 2)-zCentroid)]
     # sVec is the vector that contains s ----- yVec is the vector that contains y ----- zVecCentroid is the vector that contains z w.r.t. centroid
@@ -79,9 +140,12 @@ def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
     # stringer contribution is added afterwards
     intYVec1 = addStringerContribution(intYVec1,yVec1,zVecCentroid1,ds1,n1,stringerPosFilt1,stArea,"Y")
     intZVec1 = addStringerContribution(intZVec1,yVec1,zVecCentroid1,ds1,n1,stringerPosFilt1,stArea,"Z")
+
     qs1 = -(Sz/Iyy)*intZVec1 - (Sy/Izz)*intYVec1
 
-    ###### (2) spar plate : continuing from (1)
+    #---------------------------------------------------
+    # (2) spar plate : continuing from (1)
+    #---------------------------------------------------
     sVec2 = np.linspace(0,ha,n2+1)
     yVec2 = ha/2-sVec2
     zVecCentroid2 = np.ones(len(sVec2))*(-zCentroid -ha/2)
@@ -91,7 +155,10 @@ def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
     # add last value from qs1
     qs2 += qs1[-1]
 
-    ###### (4) semi-circular area : starting from (y = ha/2, z = -ha/2)
+    #---------------------------------------------------
+    # (4) semi-circular area : starting from (y = ha/2, z = -ha/2)
+    #---------------------------------------------------
+
     # filter stringers of semicircle
     stringerPosFilt4 = stringerPosCentroid[:, (stringerPosCentroid[1, :] >= (-ha / 2)-zCentroid)]
     thetaVec4 = np.linspace(0, np.pi, n4 + 1)
@@ -107,7 +174,10 @@ def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
     intZVec4 = addStringerContribution(intZVec4, yVec4, zVecCentroid4, ds4, n4, stringerPosFilt4, stArea, "Z")
     qs4 = -(Sz / Iyy) * intZVec4 - (Sy / Izz) * intYVec4
 
-    ##### (3) lower flat plate : continuing from (2) and (4)
+    #---------------------------------------------------
+    # (3) lower flat plate : continuing from (2) and (4)
+    #---------------------------------------------------
+
     # filter stringers of lower flate plate
     stringerPosFilt3 = stringerPosCentroid[:,(stringerPosCentroid[0, :] <= 0) & (stringerPosCentroid[1, :] <= (-ha / 2) - zCentroid)]
     sVec3 = np.linspace(0, plateLength, n3 + 1)
@@ -118,17 +188,45 @@ def calcShFlow(ha,ca,tsk,tsp, tst, hst, wst,nst,Sz,Sy,n1,n2,n3,n4):
     intYVec3 = addStringerContribution(intYVec3, yVec3, zVecCentroid3, ds3, n3, stringerPosFilt3, stArea, "Y")
     intZVec3 = addStringerContribution(intZVec3, yVec3, zVecCentroid3, ds3, n3, stringerPosFilt3, stArea, "Z")
     qs3 = -(Sz / Iyy) * intZVec3 - (Sy / Izz) * intYVec3
-
-    #add last value from qs2 to qs3
+    # add last value from qs2 to qs3
     qs3 += qs2[-1]
-    #add last value from qs4 to qs3
+    # add last value from qs4 to qs3
     qs3 += qs4[-1]
 
-    return qs1,qs2,qs3,qs4,sVec1,sVec2,sVec3,sVec4
+    #---------------------------------------------------
+    # calculating constant shear flow
+    #---------------------------------------------------
+
+    # old, using scipy.integrate.simps()
+    # rhs1 = -(-sp.integrate.simps(qs2, sVec2) / tsp + sp.integrate.simps(qs4, sVec4) / tsk)
+    # rhs2 = -(sp.integrate.simps(qs1, sVec1) / tsk + sp.integrate.simps(qs2, sVec2) / tsp + sp.integrate.simps(qs3,sVec3) / tsk)
+
+    # setting up matrix equations
+    rhs1 = -(-integrationArray(qs2, sVec2[0],sVec2[-1],n2) / tsp + integrationArray(qs4, sVec4[0],sVec4[-1],n4) / tsk)
+    rhs2 = -(integrationArray(qs1, sVec1[0],sVec1[-1],n1) / tsk + integrationArray(qs2, sVec2[0],sVec2[-1],n2) / tsp + integrationArray(qs3, sVec3[0],sVec3[-1],n3) / tsk)
+    A = np.matrix([[(np.pi * ha) / (2 * tsk) + (ha / tsp), -(ha / tsp)],
+                   [-(ha / tsp), (2 * plateLength) / (tsk) + (ha / tsp)]])
+    b = np.array([rhs1, rhs2])
+
+    # x[0]: q1 (constant shear flow of left cell)
+    # x[1]: q2 (constant shear flow of right cell)
+    # q1 and q2 are positive counter-clockwise
+    x = np.linalg.solve(A, b)
+
+    # adding constant shear flow to open section shear flow to get final shear flow
+    q1 = qs1 + x[1]
+    q2 = qs2 + x[1] - x[0]
+    q3 = qs3 + x[1]
+    q4 = qs4 + x[0]
+
+    return q1,q2,q3,q4,sVec1,sVec2,sVec3,sVec4
 
 
 def drawGraph(x,y):
-    """Helper function to draw graphs"""
+    #---------------------------------------------------
+    # Helper function to draw graphs
+    #
+    #---------------------------------------------------
     fig, ax = plt.subplots()
     plt.title("Shear Flow")
     plt.plot(x,y)
@@ -138,15 +236,27 @@ def drawGraph(x,y):
 
 
 def calcIntegralArray(z,y,s, n,t):
-    """Used in shear flow calculations"""
+    #---------------------------------------------------
+    # Evaluate integrals needed in calcShFlow()
+    #
+    # output:       description:                                type:
+    # t*intYVec     integral of (t*y*ds) from s[0] to s[-1]     array[n+1 , 1]
+    # t*intZVec     integral of (t*z*ds) from s[0] to s[-1]     array[n+1 , 1]
+    #
+    # Assuming t is constant in the section that is integrated from s[0] to s[-1]
+    #
+    #---------------------------------------------------
+
     intYVec = np.array([0])
     intZVec = np.array([0])
     for i in range(1, n+1):
-        #integralY = sp.integrate.simps(y[:i + 1], s[:i + 1])
+        # old, using sp.integrate.simps()
+        # integralY = sp.integrate.simps(y[:i + 1], s[:i + 1])
         integralY = integrationArray(y[:i+1],s[0],s[i],i)
         intYVec = np.append(intYVec, integralY)
 
-        #integralZ = sp.integrate.simps(z[:i + 1], s[:i + 1])
+        # old, using sp.integrate.simps()
+        # integralZ = sp.integrate.simps(z[:i + 1], s[:i + 1])
         integralZ = integrationArray(z[:i+1],s[0],s[i],i)
         intZVec = np.append(intZVec, integralZ)
 
@@ -154,7 +264,15 @@ def calcIntegralArray(z,y,s, n,t):
 
 
 def addStringerContribution(integrated,yVec,zVec,ds,n,stringerPos,stArea,direction):
-    """Used in shear flow calculations"""
+    #---------------------------------------------------
+    # Add stringer contribution to shear flow
+    #
+    # output:           description:                                                                    type:
+    # newIntegrated     integral of (t*y*ds) OR (t*z*ds) from s[0] to s[-1] + stringer contribution     array[n+1 , 1]
+    #
+    # Stringers are booms with point area B, added contribution is B*y OR B*z
+    #
+    #---------------------------------------------------
     used = np.array([])
     newIntegrated = integrated
     for i in range(0,n+1):
@@ -174,5 +292,13 @@ def addStringerContribution(integrated,yVec,zVec,ds,n,stringerPos,stArea,directi
 
 
 def calcDist(y1,z1,y2,z2):
-    """Euclidean norm"""
-    return np.sqrt(np.power(y1-y2, 2) + np.power(z1-z2, 2))
+    #---------------------------------------------------
+    # Calculate distance between two points (Euclidean norm)
+    #
+    # output:       description:                                type:
+    # dist          distance between the two given points       value
+    #
+    #---------------------------------------------------
+    dist = np.sqrt(np.power(y1-y2, 2) + np.power(z1-z2, 2))
+
+    return dist
